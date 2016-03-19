6401dc4335e43dd897460fee8ea0dcc41752bd3c
Stefan Andric <stex_kv@hotmail.com>
2016-03-19 10:13:58 +0100

    Adding XCode project.

---
+++ OpenWeather/OpenWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenWeather
+//
+//  Created by Stefan Andric on 3/19/16.
+//  Copyright © 2016 Stefan Andric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenWeather/OpenWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenWeather
+//
+//  Created by Stefan Andric on 3/19/16.
+//  Copyright © 2016 Stefan Andric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
